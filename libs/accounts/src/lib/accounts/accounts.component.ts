997e0dd6e4d486ab9279467ab6bca219973c368c
Abdul Shaik <nabdulsk@gmail.com>
2025-06-18 15:09:36 +0530

    create retail and business apps and accounts and transactions libs

---
+++ libs/accounts/src/lib/accounts/accounts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-accounts',
+  imports: [CommonModule],
+  templateUrl: './accounts.component.html',
+  styleUrl: './accounts.component.css',
+})
+export class AccountsComponent {}
---
+++ libs/transactions/src/lib/transactions/transactions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-transactions',
+  imports: [CommonModule],
+  templateUrl: './transactions.component.html',
+  styleUrl: './transactions.component.css',
+})
+export class TransactionsComponent {}
