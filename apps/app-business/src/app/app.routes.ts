ff63db9be8ea08b26f738febcf192e793115d953
Abdul Shaik <nabdulsk@gmail.com>
2025-06-18 16:17:48 +0530

    added a route for business app

--- apps/app-business/src/app/app.routes.ts
+++ apps/app-business/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Route } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        pathMatch: 'full',
+        children: [
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'business-accounts'
+            },
+            {
+                path: 'business-accounts',
+                loadComponent: () =>
+                    import('@accounts').then(m => m.AccountsComponent)
+            }
+        ]
+    }
+
+];
